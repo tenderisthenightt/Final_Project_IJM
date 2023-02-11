6f53f5c12e27789305b94e35370bbea3b79c466f
Donghwan Jung <yiijk700@gmail.com>
2023-02-11 22:18:16 +0900

    a

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, flash, g, redirect, url_for, 
 from werkzeug.utils import secure_filename
 import sqlite3 as sql
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #tensorflow warning 끄는 용
 
 # 1st test
 import keras.applications as kapp
